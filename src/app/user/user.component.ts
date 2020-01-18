e7a0b07e699baa373915c7173a44eeaa88f97306
arondasamuel123 <arondasamuel123@gmail.com>
2020-01-18 16:48:29 +0300

    Display name and profile photo on user component.html and imported bootstrap

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-user',
@@ -8,10 +9,13 @@ import { UserService } from '../user.service';
 })
 export class UserComponent implements OnInit {
 
+  user: User;
+
   constructor(private userService: UserService) { }
 
   ngOnInit() {
     this.userService.userRequest();
+    this.user = this.userService.user;
   }
 
 }
