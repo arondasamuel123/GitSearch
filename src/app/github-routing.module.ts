0d22a52fbb1840baa684adc4ccd1c3ad0d11804b
arondasamuel123 <arondasamuel123@gmail.com>
2020-01-19 16:50:21 +0300

    Completed search of repo component and styled the specific user list of repositories

--- src/app/github-routing.module.ts
+++ src/app/github-routing.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
+import { SearchRepoComponent } from './search-repo/search-repo.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/users', pathMatch: 'full'},
   {path: 'users', component: UserComponent},
-  {path: 'users/:username', component: UserDetailsComponent}
+  {path: 'users/:username', component: UserDetailsComponent},
+  {path:'repos',component:SearchRepoComponent}
 
 ];
 
--- src/app/search-repo/search-repo.component.ts
+++ src/app/search-repo/search-repo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
 
 @Component({
   selector: 'app-search-repo',
@@ -6,8 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-repo.component.css']
 })
 export class SearchRepoComponent implements OnInit {
+repos: any;
+s: string;
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  repoSearch() {
+    interface ApiResponse {
+      name: string;
+      html_url: string;
+      description: string;
+      created_at: Date;
+    }
+
+    const promise = new Promise((resolve, reject) => {
+      this.http.get<ApiResponse>(`${environment.searchUrl}repositories?q=${this.s}+language:javascript&sort=stars&order=desc`, {
+          headers: {
+            Authorization: `Bearer ${environment.accessToken}`
+          }
+      }).toPromise().then(response => {
+
+            this.repos = response['items'];
+          //  console.log(this.repos.id);
+            console.log(this.repos);
+            console.log(this.s);
+            resolve();
+      },
+      error => {
+        console.log('This isn\'t working');
+        reject(error);
+      });
+    });
+    return promise;
+  }
 
   ngOnInit() {
   }
